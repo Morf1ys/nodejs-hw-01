b045e8fb17deaadb4d7877f8e74bba560ba093ee
Dobrota <dobrota.art@gmail.com>
2024-05-27 20:17:30 +0300

    update

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -6,9 +6,11 @@ const generateContacts = async (number) => {
   try {
     const data = await fs.readFile(PATH_DB, 'utf-8');
     const contacts = JSON.parse(data);
+
     for (let i = 0; i < number; i++) {
       contacts.push(createFakeContact());
     }
+
     await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
     console.log(`Successfully added ${number} contacts.`);
   } catch (error) {
